f2be8d69b5e6c40e538ef14af76e56dbabf39b2b
jack-low <jack3low@gmail.com>
2012-07-18 03:38:09 +0900

    2012/07/18 更新

--- js/editor.js
+++ js/editor.js
@@ -1,7 +1,28 @@
-var strScriptPath;var strCreateFile;var text;var blobBuilder;function selct() {
-    document.OUTP.outp.value = document.INPT.inpt.value;    text = document.INPT.inpt.value;    if(document.OUTP.outp.value != ""){      var str1=document.OUTP.outp.value;      alert("compiling");            strat();		          }else {      alert("入力ミス");      }
+var strScriptPath;var strCreateFile;var text;var blobBuilder;var str1;var str2;var textList;var i;var myArray;window.onload = function () {	//textリストをサーバより取得する
+	var resText = getTextlist();
+	
+	//取得したリストを配列に格納
+	textList = resText.split("\n");	}function getTextlist() {
+
+	var resText;
+
+	req = new XMLHttpRequest();
+	req.open('GET','files/date.txt',false);
+	req.send(null);
+	resText = req.responseText;
+
+	return resText;
+}function selct() {
+         str1 = document.INPT.inpt.value;          if(str1 != ""){      alert("compiling");            // "," で切り出しを行う
+        myArray = str1.split(",");                  
+            
+
+        // 配列数分（切り出しされた文字列数分）ループして内容を表示する
+        for (i=0; i<myArray.length; i++) {                   myArray[i] =  myArray[i].replace(/test/g,textList[i]);
+           document.OUPT.oupt.value = document.OUPT.oupt.value+myArray[i];                            
+        }       document.OUPT.oupt.value = document.OUPT.oupt.value.replace(/undefined/g, "");      strat();		          }else {      alert("入力ミス");      }
 }function strat() {
-		        var text = document.INPT.inpt.value;
+		        text = document.OUPT.oupt.value;
 		        var blobBuilder;
 		        // ①BlobBuilderの作成
 		        if ("MozBlobBuilder" in window) {
@@ -23,5 +44,5 @@ var strScriptPath;var strCreateFile;var text;var blobBuilder;function selc
 		    }
 //テキストボックスの文字を削除する
 function CLR(){
-  document.INPT.inpt.value="";  document.OUTP.outp.value="";
+  document.INPT.inpt.value="";  document.OUPT.oupt.value="";
 }
